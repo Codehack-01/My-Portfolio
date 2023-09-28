d389a99eb2731281082c7041c9025ff619fe0875
Codehack <ademolaprojectdreams@gmail.com>
2023-09-28 04:05:27 +0100

    source code

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('open')
+    nav.classList.toggle('flex')
+    nav.classList.toggle('hidden')
+    
+})
+
+
+
 window.addEventListener('scroll', () => {
     const navbar = document.getElementById('gradline');
     if (window.scrollY > 0) {
