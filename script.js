851c570087c121e9443eeb612163454af01ec475
Codehack <ademolaprojectdreams@gmail.com>
2023-09-30 10:43:48 +0100

    source code

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove('bg-transparent');
     navbar.classList.add('bg-black/70', 'backdrop-blur-md', 'backdrop-filter'); // Change to the desired background color
   } else {
-    navbar.classList.remove('bg-black/70', 'backdrop-blur-md', 'backdrop-filter');
+    navbar.classList.remove('bg-black/70 backdrop-blur-md backdrop-filter');
     navbar.classList.add('bg-transparent');
   }
 });
